6f705b00c72c7d300b392071bbb7aafa75c227ca
Yi Cao <me@ycao.top>
2023-03-31 00:03:02 +0800

    Proof of Concept

--- IFS.swiftpm/System.swift
+++ IFS.swiftpm/System.swift
@@ -7,6 +7,26 @@
 
 import Foundation
 
-class AffineTramsformation {
+class IFSSystem {
+    var position: CGPoint = CGPoint(x: 0.5, y: 0.5)
+    var transformations: [CGAffineTransform] = []
     
+    convenience init() {
+        self.init(transformations: [])
+    }
+    
+    init(transformations: [CGAffineTransform]) {
+        self.transformations = transformations
+    }
+    
+    func addTransformation(iHat: CGPoint, jHat: CGPoint, shift: CGPoint) {
+        let t = CGAffineTransform(iHat.x, iHat.y, jHat.x, jHat.y, shift.x, shift.y)
+        self.transformations.append(t)
+    }
+
+    func chaosGameStep() -> CGPoint {
+        let t = transformations.randomElement()!
+        position = position.applying(t)
+        return position
+    }
 }
