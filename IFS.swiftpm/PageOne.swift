37f65a26bf0b5e62dac1cfcbd7d8a7c5939ecf0c
Yi Cao <me@ycao.top>
2023-03-30 22:46:58 +0800

    Initial commit

---
+++ IFS.swiftpm/PageOne.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct PageOne: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
---
+++ IFS.swiftpm/System.swift
@@ -0,0 +1,12 @@
+//
+//  System.swift
+//  IFS
+//
+//  Created by Kerman on 2023/3/30.
+//
+
+import Foundation
+
+class AffineTramsformation {
+    
+}
